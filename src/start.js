55b77637b88984b352d92e47b09d903eea176b94
Florent Le Gall <florent.legall@gmail.com>
2016-08-06 17:36:45 +0200

    Add command line parser

--- src/start.js
+++ src/start.js
@@ -1,4 +1,28 @@
 // @flow
+import yargs from 'yargs';
+const argv = yargs
+    .usage('Usage: $0 [protocolFile] [typeDefinitionFile] --syntax [syntax]')
+    .demand(2, 'You must provide a protocolFile and a target typeDefinitionFile')
+    .demand('syntax')
+    .alias('syntax', 's')
+    .nargs('syntax', 1)
+    .describe('syntax', 'Choose your language syntax')
+    .choices('syntax', ['flow', 'typescript'])
+    .example('$0 file.proto typeDef.js -s flow',
+        'Generate a flowtype definition file from the .proto file')
+    .example('$0 file.proto typeDef.ts -syntax typescript',
+        'Generate a typescript definition file from the .proto file')
+    .epilog('https://github.com/flegall/protobuf-js-typer')
+    .help()
+    .strict()
+    .argv;
+
+const {_: [protocolFile, typeDefinitionFile], syntax} = argv;
+
 export default function start() {
-    console.log('Hello world');
+    execute(protocolFile, typeDefinitionFile, syntax);
+}
+
+export function execute(protocolFile:string, typeDefinitionFile: string, syntax: string) {
+    
 }
